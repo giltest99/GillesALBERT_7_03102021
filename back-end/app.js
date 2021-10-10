b2d05a76e0dc747cc7c011fd2dac2a38faadcf1d
gilles <sellig.blater@free.com>
2021-10-10 19:02:29 +0200

    Work in progress...

--- back-end/app.js
+++ back-end/app.js
@@ -4,35 +4,36 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require("helmet");
 const path = require('path');
+const { Sequelize } = require('sequelize');
 
+const app = express();
 
 // Routes import
-
+const userRoutes = require("./routes/user");
+const postRoutes = require("./routes/post");
+const commentRoutes = require("./routes/comment");
+const likeRoutes = require("./routes/like");
 
 
 // Database connect
 
 
-// Cors
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
 
 
 // Middlewares
 app.use(helmet());
 app.use(express.json());
-
+app.use(cors());
 
 // Routes
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
 
 
 // Static files
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 module.exports = app;
