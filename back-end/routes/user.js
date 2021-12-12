9a58221493d78b264e93c4021897d0f283eaa1d4
Gilles <sellig.blater@free.fr>
2021-12-12 16:47:36 +0100

    Fix multer middleware wrong file name

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Middlewares
 const auth = require('../middleware/auth');
-//const multer = require('../middleware/multer-config'); 
+const multer = require('../middleware/multer-config'); 
 
 // Controller
 const userCtrl = require('../controllers/user');
@@ -11,13 +11,13 @@ const userCtrl = require('../controllers/user');
 // Endpoints
 router.get('/', userCtrl.selectAllUsers);
 router.get('/id/:id', userCtrl.selectUserById);
-router.get('/username/:username', userCtrl.selectUserByUserName);
-router.get('/email/:email', userCtrl.selectUserByUserEmail);
+
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.delete('/:id', userCtrl.deleteUser);
-router.put('/:id', userCtrl.updateUser);
+router.put('/:id', multer, userCtrl.updateUser);
 
-
+//router.get('/username/:username', userCtrl.selectUserByUserName);
+//router.get('/email/:email', userCtrl.selectUserByUserEmail);
 
 module.exports = router;
