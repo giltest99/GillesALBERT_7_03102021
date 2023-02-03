4e01a8e858c1afdb2d497441c5e0c0f954547a74
Gilles <sellig.blater@free.fr>
2023-02-03 21:01:45 +0100

    Misc fix

--- client-react/src/components/ButtonStandard.js
+++ client-react/src/components/ButtonStandard.js
@@ -1,10 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function ButtonStandard({ txt, onClick }) {
+export default function ButtonStandard({ txt, onClick,type }) {
   return (
     <>
-      <Button onClick={onClick}>{txt}</Button>
+      <Button onClick={onClick} type={type}>{txt}</Button>
     </>
   );
 }
