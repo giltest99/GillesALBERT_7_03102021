efb464e913109b1ae2790b670cdeb920a5a1ae01
Gilles <sellig.blater@free.fr>
2023-02-12 21:19:59 +0100

    Clean project files

--- front-end/src/components/BackEndError.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-const BackError = styled.p`
-  color: crimson;
-  font-style: italic;
-`;
-
-export default BackError
--- front-end/src/components/SpanError.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-const TextError = styled.span`
-  color: crimson;
-  font-style: italic;
-`;
-
-export default TextError
